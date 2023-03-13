0993ce75efcb315beba31c7cd67e0dbcf7a2ee9d
Roman Rivas II <74742067+RomanRII@users.noreply.github.com>
2023-03-13 10:36:37 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+from helper import doApi
+
+def main():
+	# Create an ArgumentParser object
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--apikey", required=True)
+	parser.add_argument("--requesteddomain", required=True)
+	parser.add_argument("--rootdomain", required=True)
+	parser.add_argument("--requiredemail", required=True)
+	# Parse the command-line arguments
+	args = parser.parse_args()
+	doAPIKey = args.apikey
+	requestedDomain = args.requesteddomain
+	rootDomain = args.rootdomain
+	requiredEmail = args.requiredemail
+	doApi.entry(doApiKey=doAPIKey, requestedDomain=requestedDomain, rootDomain=rootDomain, requiredEmail=requiredEmail)
+	# python3 .\main.py --apikey "DOAPI" --requesteddomain "@" --rootdomain "romanrii.com" --requiredemail "roman.rivas@romanrii.com"
+
+if __name__ == "__main__":
+	main()
